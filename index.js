77d992e2d4b9d5b82a2be5d9c9a45875b16d21ba
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-19 03:51:25 -0500

    Completed Lab!

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    return Math.abs(start - 42)
+}
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+  }
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    console.log(distance)
+    switch (true) {
+        case distance<=400:
+            return 0
+        case distance<=2000 && distance>400:
+            return (distance-400)*0.02
+            debugger
+        case distance<=2500:
+            return 25
+        case distance>2500:
+            return 'cannot travel that far'
+        default: return "ERROR"
+       }
+}
+calculatesFarePrice(32, 34)
